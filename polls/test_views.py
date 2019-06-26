0bc94fca415746080a34cf44202a3bed9db5e31e
yichingl <sporkdefork@MacBook-Pro-4.fios-router.home>
2019-06-26 15:31:39 -0400

    removed naive date usage in polls View tests; upgaded index view to display 5 most recent posts, and passed tests

--- polls/test_views.py
+++ polls/test_views.py
@@ -6,12 +6,19 @@ from django.urls import reverse
 
 from polls.models import Question, Choice
 
-from datetime import date
+from django.utils import timezone
 
-class PollsIndexViewTest(TestCase):
+class QuestionsIndexViewTest(TestCase):
     @classmethod
     def setUpTestData(cls):
-        pass;
+
+        # create set of Questions with known pub_dates
+        num_questions = 8
+        for q_pk in range(1,num_questions+1):
+            Question.objects.create(
+                question_text = 'Q?' + str(q_pk),
+                pub_date = timezone.now()
+            )
 
     def test_view_url_exists_at_desired_location(self):
         response = self.client.get('/polls/')
@@ -21,6 +28,15 @@ class PollsIndexViewTest(TestCase):
         response = self.client.get(reverse('polls:index'))
         self.assertEqual(response.status_code, 200)
 
+    def test_index_displays_5_questions(self):
+        response = self.client.get(reverse('polls:index'))
+        self.assertTrue(len(response.context['latest_question_list']), 5)
+
+    def test_index_questions_ordered_by_pub_date(self):
+        response = self.client.get(reverse('polls:index'))
+        latest_question_list = response.context['latest_question_list']
+        self.assertGreater(latest_question_list[0].pub_date, latest_question_list[4].pub_date)
+
 class QuestionDetailViewTest(TestCase):
 
     @classmethod
@@ -28,7 +44,7 @@ class QuestionDetailViewTest(TestCase):
         # create Question data
         Question.objects.create(
             question_text = 'this is the question?',
-            pub_date = date(2018, 7, 20)
+            pub_date = timezone.now()
         )
 
     def test_view_url_exists_at_desired_location(self):
@@ -47,7 +63,7 @@ class QuestionResultsViewTest(TestCase):
         # create Question data
         Question.objects.create(
             question_text = 'this is the question?',
-            pub_date = date(2018, 7, 20)
+            pub_date = timezone.now()
         )
 
     def test_view_url_exists_at_desired_location(self):
@@ -66,7 +82,7 @@ class QuestionVoteViewTest(TestCase):
         # create Question data
         Question.objects.create(
             question_text = 'this is the question?',
-            pub_date = date(2018, 7, 20)
+            pub_date = timezone.now()
         )
 
     def test_view_url_exists_at_desired_location(self):
--- polls/views.py
+++ polls/views.py
@@ -1,12 +1,21 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-
 from django.http import HttpResponse
 
+from django.shortcuts import render
+from django.template import loader
+
+from models import Question, Choice
+
 def index(request):
-    return HttpResponse("Hello!  Polls index.")
+    template = loader.get_template('index.html')
+
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request));
 
 def detail(request, question_pk):
     return HttpResponse("You're looking at question %s" % question_pk)
