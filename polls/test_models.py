400e57422e943953e324fdb427ce67c2d1515d2d
yichingl <sporkdefork@MacBook-Pro-4.fios-router.home>
2019-06-26 13:46:47 -0400

    minor change: added polls app in settings in using slightly different syntax

--- polls/test_models.py
+++ polls/test_models.py
@@ -17,17 +17,17 @@ class QuestionModelTest(TestCase):
         )
 
     def test_question_label(self):
-        question = Question.objects.get(id=1)
+        question = Question.objects.get(pk=1)
         field_label = question._meta.get_field('question_text').verbose_name
         self.assertEquals(field_label, 'question text')
 
     def test_question_length(self):
-        question = Question.objects.get(id=1)
+        question = Question.objects.get(pk=1)
         max_length = question._meta.get_field('question_text').max_length
         self.assertEquals(max_length, 200)
 
     def test_pub_date_label(self):
-        question = Question.objects.get(id=1)
+        question = Question.objects.get(pk=1)
         field_label = question._meta.get_field('pub_date').verbose_name
         self.assertEquals(field_label, 'date published')
 
@@ -43,31 +43,31 @@ class ChoiceModelTest(TestCase):
 
         # create Choice data
         Choice.objects.create(
-            question = Question.objects.get(id=1),
+            question = Question.objects.get(pk=1),
             choice_text = 'this is a choice.'
         )
 
     def test_question_label(self):
-        choice = Choice.objects.get(id=1)
+        choice = Choice.objects.get(pk=1)
         field_label = choice._meta.get_field('question').verbose_name
         self.assertEquals(field_label, 'question')
 
     def test_choice_text_label(self):
-        choice = Choice.objects.get(id=1)
+        choice = Choice.objects.get(pk=1)
         field_label = choice._meta.get_field('choice_text').verbose_name
         self.assertEquals(field_label, 'choice text')
 
     def test_choice_text_length(self):
-        choice = Choice.objects.get(id=1)
+        choice = Choice.objects.get(pk=1)
         max_length = choice._meta.get_field('choice_text').max_length
         self.assertEquals(max_length, 200)
 
     def test_votes_label(self):
-        choice = Choice.objects.get(id=1)
+        choice = Choice.objects.get(pk=1)
         field_label = choice._meta.get_field('votes').verbose_name
         self.assertEquals(field_label, 'votes')
 
     def test_votes_default_value(self):
-        choice = Choice.objects.get(id=1)
+        choice = Choice.objects.get(pk=1)
         expected_default_choice_value = 0
         self.assertEquals(expected_default_choice_value, choice.votes)
