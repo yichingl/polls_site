5d4272b317132c1bd2b49665e0d72c9384e5f06b
yichingl <sporkdefork@MacBook-Pro-4.fios-router.home>
2019-06-26 11:28:08 -0400

    added polls: basic tests

--- polls/tests.py
+++ polls/tests.py
@@ -2,5 +2,17 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class PollsViewTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        pass;
+
+    def test_view_url_exists_at_desired_location(self):
+        response = self.client.get('/polls/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_url_accessible_by_name(self):
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(response.status_code, 200)
