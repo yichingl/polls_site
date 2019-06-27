19f786712bcc8346a8ff0b1f30a0202e837aa83b
yichingl <sporkdefork@MacBook-Pro-4.fios-router.home>
2019-06-27 09:58:46 -0400

    added function to parse geography from json response object

--- polls/parse_files/parse_str.py
+++ polls/parse_files/parse_str.py
@@ -1,19 +1,55 @@
 import urllib2
 import os
+import re
 
 
 def read_url_data(url):
-    """ Reads data at given url and returns a string containing contents. """
+    """ Reads data at given url and returns response object. """
 
     try:
         response = urllib2.urlopen(url)
-        data_str = response.read().replace("\r","").replace("\n","")
-        return data_str
+        return response
     except urllib2.URLError as e:
         print(e.reason)
     except urllib2.HTTPError as e:
         print(e.code)
         print(e.read())
+
+def _parse_line(line, regex_dict):
+    """Matches regex dict keys with line contents, and returns key, match. """
+
+    for key, regex_obj in regex_dict.items():
+        match = regex_obj.search(line)
+        if match:
+            return key, match
+    return None, None
+
+def parse_for_states(response):
+    """ Parses given response object and returns a dict containing
+        entry# and state as key-value pairs. """
+
+    states_dict = {}
+
+    regex_dict = {
+    # "Geography": "(.+?)"
+    # "Geography": "(?P<geography>.+?)"
+        'geography': re.compile(r'"Geography": "(?P<geography>.+?)"')
+    }
+
+    line = response.readline()
+
+    cnt = 1;
+    while line:
+        key, match = _parse_line(line, regex_dict)
+
+        if match:
+            states_dict[str(cnt)] = match.group('geography')
+            cnt+=1;
+
+        # increment
+        line = response.readline()
+
+    return states_dict
 
 
 
@@ -23,5 +59,9 @@ if __name__ == '__main__':
     abs_url = os.path.abspath(rel_url)
     url_local = "file://" + abs_url
 
-    data_str = read_url_data(url_local)
-    print(data_str)
+    response = read_url_data(url_local)
+    states_dict = parse_for_states(response)
+    expected_states_dict = {"1":"California", "2":"California",
+        "3":"New York", "4":"New York", "5":"Pennsylvania"}
+    print(states_dict)
+    print(states_dict == expected_states_dict)
--- polls/parse_files/test_parse_str.py
+++ polls/parse_files/test_parse_str.py
@@ -1,19 +1,27 @@
 import unittest
 import os
 # from additional_test_files.parse_str import read_url_data
-from parse_str import read_url_data
+from parse_str import read_url_data, parse_for_states
 
 class ParseStrTestCase(unittest.TestCase):
     def setUp(self):
 
+        # Online data
+        url_online = 'https://bbotllc.github.io/candidate-interviews/political_leanings.json'
+        self.online_text = read_url_data(url_online).read().replace("\r","").replace("\n","")
+
+        # Local data
         rel_url = "parse_files/sample_text.json"
         abs_url = os.path.abspath(rel_url)
         url_local = "file://" + abs_url
 
-        url_online = 'https://bbotllc.github.io/candidate-interviews/political_leanings.json'
+        #  For checking text content
+        response = read_url_data(url_local)
+        self.local_text = response.read().replace("\r","").replace("\n","")
 
-        self.local_text = read_url_data(url_local)
-        self.online_text = read_url_data(url_online)
+        # For checking other
+        response = read_url_data(url_local)
+        self.states_dict = parse_for_states(response)
 
     def tearDown(self):
         pass;
@@ -27,6 +35,11 @@ class ParseStrTestCase(unittest.TestCase):
         expected_text_excerpt = '[ {   "Geography": '
         read_text_excerpt = self.online_text[0:len(expected_text_excerpt)]
         self.assertEqual(expected_text_excerpt, read_text_excerpt)
+
+    def test_parse_for_states(self):
+        states_dict = {"1":"California", "2":"California",
+            "3":"New York", "4":"New York", "5":"Pennsylvania"}
+        self.assertEqual(states_dict, self.states_dict)
 
 if __name__ == '__main__':
     unittest.main()
